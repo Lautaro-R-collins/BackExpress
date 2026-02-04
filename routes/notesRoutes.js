5e6809733a6fc5f89c3f5fef2eed945a5b573af2
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2026-02-04 20:32:43 -0300

    ref

--- routes/notesRoutes.js
+++ routes/notesRoutes.js
@@ -6,4 +6,18 @@ router.get('/', (req, res) => {
     res.send('Notes routes get');
 });
 
+// obtener notas por id
+router.get('/:id', (req, res) => {
+    console.log(`nota obteida por id: ${id}`);
+});
+
+//crear una nota
+router.post('/', (req, res) => {
+    res.send('Nota creada');
+});
+// eliminar nota 
+router.delete('/:id', (req, res) => {
+    res.send('Nota eliminada');
+});
+
 export default router;
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import router from './routes/notesRoutes.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use('/notes', router);
