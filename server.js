56fb3b9270a670dc6c5b4b08ad26beb622147dd4
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2026-02-04 20:18:12 -0300

    add routes

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import express from 'express';
+import router from './routes/notesRoutes.js';
 
 const app = express();
 const PORT = 3000;
 
+// Middleware
+app.use('/notes', router);
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.status(201).json({ message: 'Servidor funcionando correctamente' });
 });
 
 app.listen(PORT, () => {
